9175ad5ab477f6702a720f0b36038808ca710531
Ansleigh <ansleighong@gmail.com>
2026-01-06 14:07:05 +0800

    Initial project setup with calculator app

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="calculator-app",
+    version="0.1.0",
+    description="Calculator with CI/CD",
+    author="Ansleigh",
+    packages=find_packages(),
+    python_requires=">=3.9",
+)
